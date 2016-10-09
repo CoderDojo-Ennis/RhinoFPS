66847c06e4811fdb028fcfdff5492548fb91816a
Quinn Painter <quinn@geekymonkey.com>
2016-10-09 16:13:57 +0100

    character

---
+++ RhinoFPS/Assets/CharacterControl.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterControl : MonoBehaviour
+{
+	void Update ()
+    {
+        Vector3 MoveVector = new Vector3();
+        if (Input.GetKey(InputController.Forward))
+        {
+
+        }
+        GetComponent<CharacterController>().SimpleMove(MoveVector);
+    }
+}
---
+++ RhinoFPS/Assets/InputController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputController : MonoBehaviour
+{
+    public static KeyCode Forward = KeyCode.W;
+    public static KeyCode Back = KeyCode.S;
+    public static KeyCode Left = KeyCode.A;
+    public static KeyCode Right = KeyCode.D;
+
+    void Start ()
+    {
+	
+	}
+}
