e1c968ae9c340199903d34c107fc92e6592aacbe
Quinn Painter <quinn@geekymonkey.com>
2016-10-09 17:57:46 +0100

    Basic multiplayer

--- RhinoFPS/Assets/CharacterControl.cs
+++ RhinoFPS/Assets/CharacterControl.cs
@@ -1,15 +1,40 @@
 ﻿using UnityEngine;
+using UnityEngine.Networking;
 using System.Collections;
 
-public class CharacterControl : MonoBehaviour
+public class CharacterControl : NetworkBehaviour
 {
-	void Update ()
-    {
-        Vector3 MoveVector = new Vector3();
-        if (Input.GetKey(InputController.Forward))
-        {
+    public float SpeedMultiplier;
 
+    Color randColor;
+    public override void OnStartLocalPlayer()
+    {
+        randColor = new Color(Random.value, Random.value, Random.value);
+        GetComponent<MeshRenderer>().material.color = randColor;
+    }
+
+    void FixedUpdate ()
+    {
+        if (isLocalPlayer)
+        {
+            Vector3 MoveVector = new Vector3();
+            if (Input.GetKey(InputController.Forward))
+            {
+                MoveVector.z += SpeedMultiplier;
+            }
+            if (Input.GetKey(InputController.Back))
+            {
+                MoveVector.z -= SpeedMultiplier;
+            }
+            if (Input.GetKey(InputController.Left))
+            {
+                MoveVector.x -= SpeedMultiplier;
+            }
+            if (Input.GetKey(InputController.Right))
+            {
+                MoveVector.x += SpeedMultiplier;
+            }
+            transform.Translate(MoveVector);
         }
-        GetComponent<CharacterController>().SimpleMove(MoveVector);
     }
 }
